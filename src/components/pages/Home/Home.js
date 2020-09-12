f75ca94833cf95ad7700245f6bbed76a0d533633
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-09-12 16:15:27 -0500

    more routing and components

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
-  render() {
-    return (
+editActivityEvent = (e) => {
+  e.preventDefault();
+  const activityId = 'coolActivity01';
+  this.props.history.push(`/edit/${activityId}`);
+}
+
+render() {
+  return (
             <div className="Home">
                 <h1>Home</h1>
+                <button className="btn btn-dark" onClick={this.editActivityEvent}>Edit Activity</button>
+                <Link to='/newActivity'>New Activity</Link>
+                <h2>Here is a link to a <Link to='/activity/activity01'>Specific Activity</Link> </h2>
             </div>
-    );
-  }
+  );
+}
 }
 
 export default Home;
