96d09ceddd104c0befb14f6405d787b8ac92f8d6
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-09-12 14:07:57 -0500

    wip

--- src/components/pages/EditActivity/EditActivity.js
+++ src/components/pages/EditActivity/EditActivity.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 class EditActivity extends React.Component {
   render() {
+    const { activityId } = this.props.match.params;
     return (
             <div className="EditActivity">
-                <h1>Edit Activity</h1>
+                <h1>You are editing Activity: {activityId}</h1>
             </div>
     );
   }
