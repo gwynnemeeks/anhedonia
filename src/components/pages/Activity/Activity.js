740a9551c15cc3f85c008ea9212d6c7c82cd6ff8
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-09-14 21:21:54 -0500

    still not working wip

--- src/components/pages/Activity/Activity.js
+++ src/components/pages/Activity/Activity.js
@@ -4,18 +4,18 @@ import activityShape from '../../../helpers/propz/activityShape';
 
 class ActivityCards extends React.Component {
     static propTypes = {
-      activity: activityShape.activityShape,
+      activities: activityShape.activityShape,
     }
 
     render() {
-      const { activity } = this.props;
+      const { activities } = this.props;
 
       return (
             <div className="card text-center">
-        <div className="card-header"><h5>{activity.name}</h5></div>
+        <div className="card-header"><h5>{activities.name}</h5></div>
         <div className="card-body">
-          <p className="card-title">{activity.timesPerWeekGoal}</p>
-          <p className="card-text">{activity.description}</p>
+          <p className="card-title">{activities.timesPerWeekGoal}</p>
+          <p className="card-text">{activities.description}</p>
           <button className="btn btn-secondary">View activity</button>
           </div>
           </div>
--- src/helpers/data/activityData.js
+++ src/helpers/data/activityData.js
@@ -1,15 +1,27 @@
 import axios from 'axios';
 
-import utils from '../utils';
-
 import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseConfig.databaseURL;
 
 const getActivitiesByUid = (uid) => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/activities.json?orderBy="uid"&equalTo="${uid}"`)
-    .then(({ data }) => resolve(utils.convertFirebaseCollection(data)))
+  axios.get(`${baseUrl}/activity.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => {
+      const allActivities = response.data;
+      const myActivity = [];
+
+      if (allActivities) {
+        Object.keys(allActivities).forEach((activityId) => {
+          const activity = allActivities[activityId];
+          activity.id = activityId;
+          myActivity.push(activity);
+        });
+      }
+
+      resolve(myActivity);
+    })
     .catch((err) => reject(err));
 });
+console.warn(getActivitiesByUid);
 
 export default { getActivitiesByUid };
