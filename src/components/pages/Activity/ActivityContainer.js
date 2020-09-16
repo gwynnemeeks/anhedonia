bf3f938b872fc3d7b30f7e1223f1c8cd61c0edf6
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-09-16 10:56:17 -0500

    get and read activities refactor and nothing broken yet

--- src/components/pages/Activity/ActivityContainer.js
+++ src/components/pages/Activity/ActivityContainer.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import Activity from './Activity';
+
+import authData from '../../../helpers/data/authData';
+import activityData from '../../../helpers/data/activityData';
+
 class ActivityContainer extends React.Component {
-  render() {
-    return (
-            <h2>Activity Cards Here</h2>
-    );
-  }
+    state = {
+      activities: [],
+    }
+
+    componentDidMount() {
+      activityData.getActivitiesByUid(authData.getUid())
+        .then((activities) => this.setState({ activities }))
+        .catch((err) => console.error('get activities sucks', err));
+    }
+
+    render() {
+      const { activities } = this.state;
+
+      const activityCard = activities.map((activity) => <Activity key={activity.id} activity={activity} />);
+      return (
+            <div className="card-columns">
+                {activityCard}
+            </div>
+      );
+    }
 }
 
 export default ActivityContainer;
