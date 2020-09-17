2e69de6e6271790905326238d58d7ae8dffc7deb
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-09-17 18:23:18 -0500

    wip change events

--- src/components/pages/NewActivity/NewActivity.js
+++ src/components/pages/NewActivity/NewActivity.js
@@ -1,6 +1,61 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import authData from '../../../helpers/data/authData';
 
 class NewActivity extends React.Component {
+  static propTypes = {
+    createActvity: PropTypes.func.isRequired,
+  }
+
+  state = {
+    name: '',
+    description: '',
+    timesPerWeekGoal: '',
+    isArchived: '',
+  }
+
+  changeNameEvent = (e) => {
+    e.preventDefault();
+    this.setState({ name: e.target.value });
+  }
+
+  changeDescriptionEvent = (e) => {
+    e.preventDefault();
+    this.setState({ description: e.target.value });
+  }
+
+  changeTimesPerWeekGoalEvent = (e) => {
+    e.preventDefault();
+    this.setState({ timesPerWeekGoal: e.target.value });
+  }
+
+  changeIsArchievedEvent = (e) => {
+    e.preventDefault();
+    this.setState({ isArchived: e.target.checked });
+  }
+
+  saveActvitiyEvent = (e) => {
+    e.preventDefault();
+    const {
+      name,
+      description,
+      timesPerWeekGoal,
+      isArchived,
+    } = this.state;
+    const { createActvity } = this.props;
+
+    const newActivity = {
+      name,
+      description,
+      timesPerWeekGoal,
+      isArchived,
+      uid: authData.getUid(),
+    };
+    createActvity(newActivity);
+    console.warn('here is a new activity', newActivity);
+  }
+
   render() {
     return (
             <div className="NewActivity">
