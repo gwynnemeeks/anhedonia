47e53265081090f81b98cd0d06c3ff7d6d2e467b
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-09-17 20:47:51 -0500

    wip

--- src/components/pages/NewJournalEntry/NewJournalEntry.js
+++ src/components/pages/NewJournalEntry/NewJournalEntry.js
@@ -74,9 +74,69 @@ class NewJournalEntry extends React.Component {
 
   render() {
     return (
-            <div className="NewJournalEntry">
-                <h1>New Journal Entry</h1>
-            </div>
+      <form className="col-6 offset-3">
+      <div className="form-group">
+      <label htmlFor="activityName">Activity Name</label>
+      <input
+      type="text"
+      className="form-control"
+      id="activityName"
+      placeholder="Enter Activity Name"
+      onChange={this.changeActivityNameEvent}
+    />
+  </div>
+  <div className="form-group">
+    <label htmlFor="date">Journal Entry Date</label>
+    <input
+      type="text"
+      className="form-control"
+      id="date"
+      placeholder="Enter date in the following format: MM.DD.YYYY"
+      onChange={this.changeDateEvent}
+    />
+  </div>
+  <div className="form-group">
+    <label htmlFor="Mood">Your Current Mood</label>
+    <input
+      type="text"
+      className="form-control"
+      id="Mood"
+      placeholder="Current Mood"
+      onChange={this.changeMoodEvent}
+    />
+  </div>
+  <div className="form-group">
+            <label htmlFor="moodIcon">Mood Icon</label>
+            <input
+            type="text"
+            className="form-control"
+            id="moodIcon"
+            placeholder="Enter Font Awesome Icon here and add fa-3x"
+            onChange={this.changeMoodIconEvent}
+          />
+        </div>
+  <div className="form-group">
+    <label htmlFor="moodColor">Mood Color</label>
+    <input
+      type="text"
+      className="form-control"
+      id="moodColor"
+      placeholder="Team Name"
+      onChange={this.changeMoodColorEvent}
+    />
+    <div className="form-group">
+    <label htmlFor="entryText">Entry Text</label>
+    <input
+      type="textarea"
+      className="form-control"
+      id="entryText"
+      placeholder="Journal Entry Text"
+      onChange={this.changeEntryTextEvent}
+    />
+  </div>
+  <button className="btn btn-dark" onClick={this.saveJournalEntryEvent}><i className="fas fa-save"></i></button>
+</div>
+</form>
     );
   }
 }
