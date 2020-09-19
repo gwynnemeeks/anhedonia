a0e9eeb82dbc075abe71d275f7504d0805b1d0f1
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-09-19 09:36:18 -0500

    wip before test

--- src/components/pages/Activity/ActivityContainer.js
+++ src/components/pages/Activity/ActivityContainer.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Activity from './Activity';
 import NewActivity from '../NewActivity/NewActivity';
+import EditActivity from '../EditActivity/EditActivity';
 
 import authData from '../../../helpers/data/authData';
 import activityData from '../../../helpers/data/activityData';
@@ -16,6 +17,8 @@ class ActivityContainer extends React.Component {
     state = {
       activities: [],
       formOpen: false,
+      isEditing: false,
+      editActivity: {},
     }
 
     getActivity = () => {
@@ -45,16 +48,36 @@ class ActivityContainer extends React.Component {
         .catch((err) => console.error('create activity went awry', err));
     }
 
+    editAnActivity = (activityToEdit) => {
+      this.setState({ isEditing: true, editActivity: activityToEdit });
+    }
+
+    updateActivity = (activityId, updatedActivity) => {
+      activityData.updateActivity(activityId, updatedActivity)
+        .then(() => {
+          this.getActivity();
+          this.setState({ isEditing: false, editActivity: {} });
+        })
+        .catch((err) => console.error('update activity sucks', err));
+    }
+
     render() {
-      const { activities, formOpen } = this.state;
+      const {
+        activities,
+        formOpen,
+        isEditing,
+        editActivity,
+      } = this.state;
       const { setSingleActivity } = this.props;
 
-      const activityCard = activities.map((activity) => <Activity key={activity.id} activity={activity} setSingleActivity={setSingleActivity} deleteActivity={this.deleteActivity} />);
+      const activityCard = activities.map((activity) => <Activity key={activity.id} activity={activity} setSingleActivity={setSingleActivity}
+      deleteActivity={this.deleteActivity} editAnActivity={this.editAnActivity} />);
 
       return (
   <div>
-        <button className="btn btn-warning" onClick={() => { this.setState({ formOpen: !formOpen }); }}><i className="far fa-plus-square fa-lg">New Activity</i></button>
+        <button className="btn btn-warning" onClick={() => { this.setState({ formOpen: !formOpen }); }}><i className="far fa-plus-square fa-lg"></i></button>
       { formOpen ? <NewActivity createActivity={this.createActivity}/> : '' }
+      { isEditing ? <EditActivity editAnActivity={editActivity} updateActivity={this.updateActivity} /> : '' }
             <div className="card-columns">
                 {activityCard}
             </div>
